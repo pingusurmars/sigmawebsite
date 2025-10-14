f0ecb5b6df804edeb659bbd34a2998a0d58a5b5c
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-14 21:41:07 +0200

    webstorm is mad i'm not putting title in the app html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,6 @@
     <link rel="icon" type="image/png" sizes="32x32" href={favicon32}>
     <link rel="icon" type="image/png" sizes="16x16" href={favicon16}>
     <link rel="manifest" href={manifest}>
-    <title>PinguSurMars's Website</title>
 </svelte:head>
 
 {@render children?.()}
