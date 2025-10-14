9d18c48362ad96219a7375e93321df495707953a
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 00:26:40 +0200

    yap yap yap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
     import Navbar from "$lib/components/Navbar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+
 </script>
 
 <Navbar />
+<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <p class="text-white text-6xl">Hi!👋</p>
+    <p class="text-white py-4">I'm PinguSurMars_ (or Pingu for short) and I make stuff in whatever language speaks the most to my soul.</p>
+    <p class="text-white py-2">I mostly game or watch YouTube in my free time, and I sometimes decide randomly to code random stuff like this website for no particular reason. (I'm writing this at 00:11 CEST on a Wednesday Morning with class the next day)</p>
+    <p class="text-white py-2"><a class="underline decoration-1" href="https://github.com/pingusurmars/docker-compose-stack">I also run a few services</a> here and there on an old PC that I call my "server" (he's old but works OK-ish for what I do)</p>
+    <p class="text-white py-4">That's pretty much it, feel free to look around here in the other pages! (it's not like I can prevent you from doing it)</p>
+</div>
+<Footer />
