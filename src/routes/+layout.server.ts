f67bd95506a83214b47b978102874cc95ff029ae
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 17:30:38 +0200

    small little simplification

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,19 +1,9 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function load({ request }: RequestEvent) {
-    const headers = request.headers;
-
-    // Convert to an object or array
-    const headersObject: Record<string, string> = {};
-    headers.forEach((value, key) => {
-        headersObject[key] = value;
-    });
-
-    // Or as an array of [key, value] pairs
-    const headersArray = Array.from(headers.entries());
+    const headersArray = Array.from(request.headers.entries());
 
     return {
-        headers: headersObject,
-        headersArray: headersArray
+        headers: headersArray
     };
 }
