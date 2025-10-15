b0b68938def8c228e3f6e5a8e0b6f1882e2e1e70
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 19:24:45 +0200

    it's always the damn padding

--- src/routes/utilities/whats-my-ip/+page.svelte
+++ src/routes/utilities/whats-my-ip/+page.svelte
@@ -20,15 +20,15 @@
     <p class="text-white py-6 text-xl decoration-1">
         Your IP address is <strong>{ipAddr}</strong>
     </p>
-    <p class="text-white py-4">Here's some info from <a href="http://ip-api.com">IP-API.com</a></p>
-    <p class="text-white py-2">City: {ipInfo.city}</p>
+    <p class="text-white py-4 underlined decoration-1">Here's some info from <a href="http://ip-api.com">IP-API.com's API: </a></p>
+    <p class="text-white">City: {ipInfo.city}</p>
     <p class="text-white">ZIP Code: {ipInfo.zip}</p>
     <p class="text-white">Region: {ipInfo.regionName}</p>
     <p class="text-white">Country: {ipInfo.country}</p>
     <p class="text-white">ISP: {ipInfo.isp}</p>
     <p class="text-white">ASN: {ipInfo.as}</p>
 
-    <span id="mango" class="py-2" style="display: none">Congrats on supporting IPv6 and welcome to the future's internet!</span>
+    <span id="mango" class="py-4" style="display: none">Congrats on supporting IPv6 and welcome to the future's internet!</span>
     <p class="text-white py-1">
         If you'd like to get more info about your IP address, visit
         <a class="underline decoration-1" href="https://ipinfo.io/{ipAddr}">this</a> website (info is provided as-is and may be inaccurate)
