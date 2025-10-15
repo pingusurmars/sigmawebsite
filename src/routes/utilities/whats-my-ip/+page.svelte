d9f827a408fffad78fa5e514ec1e97c3dd3a2155
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 18:47:05 +0200

    i'm starting to hate typescript

--- src/routes/utilities/whats-my-ip/+page.svelte
+++ src/routes/utilities/whats-my-ip/+page.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-    let {data} = $props();
-    const ipAddr = data.headers.find(([key]) => key === 'CF-Connecting-IP')?.[1];
+    import { onMount } from 'svelte';
+
+    let { data } = $props();
+    const ipAddr = data.headers.find(([key]) => key === 'x-real-ip')?.[1];
+    if (ipAddr === null) {
+        throw new Error('No IP address found. Something went (very) wrong.');
+    }
+
+    onMount(() => {
+        const mango = document.getElementById('mango');
+        if (ipAddr?.includes(':')) {
+            mango?.removeAttribute('style');
+        }
+    });
 </script>
 
 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-    <!-- CF adds headers when passing HTTP requests through their proxy, see https://developers.cloudflare.com/fundamentals/reference/http-headers/ for more info -->
     <p class="text-white text-6xl">What's my IP ❓</p>
-    <p class="text-white py-6 text-xl decoration-1" >Your IP address is <strong>{ipAddr}</strong></p>
-    <p class="text-white py-1">If you'd like to get more info about your IP address, visit <a class="underline decoration-1" href="https://ipinfo.io/{ipAddr}">this</a> website (not sponsored, I just like their UI)</p>
+    <p class="text-white py-6 text-xl decoration-1">
+        Your IP address is <strong>{ipAddr}</strong>
+    </p>
+    <span id="mango" style="display: none">Congrats on using IPv6!</span>
+    <p class="text-white py-1">
+        If you'd like to get more info about your IP address, visit
+        <a class="underline decoration-1" href="https://ipinfo.io/{ipAddr}">this</a> website
+    </p>
 </div>
