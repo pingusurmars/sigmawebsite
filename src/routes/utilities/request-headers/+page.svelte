636f89a3275019783355c9fe29ac42ff9937f2b4
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 17:36:42 +0200

    made this guy readable and not look like a page with failed css

--- src/routes/utilities/request-headers/+page.svelte
+++ src/routes/utilities/request-headers/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-    let { data } = $props();
+    let {data} = $props();
 </script>
 
-<ul>
-    {#each data.headers as [key, value]}
-        <li><strong>{key}:</strong> {value}</li>
-    {/each}
-</ul>
+<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <p class="text-white text-6xl">My Request Headers ❓</p>
+    <ul class="py-3 list-inline list-inside mx-auto">
+        {#each data.headers as [key, value]}
+            <li class="text-white py-1" ><strong>{key}:</strong> {value}</li>
+        {/each}
+    </ul>
+</div>
