e0a4d8742d3423c2cebca71a87d7d1b268a1ea9b
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 13:29:17 +0200

    prepared donate page

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -7,5 +7,9 @@
 <Navbar />
 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
     <p class="text-white text-6xl">Donate 💲</p>
+    <p class="text-white py-4">First of all, thanks for being here (which I'm guessing means you'd want to support me financially) and checking this page out.</p>
+    <p class="text-white py-2">Second of all, I stopped accepting PayPal pretty long ago because of KYC rules & all that stuff (and I'd rather not send my ID to a company in another country without knowing where my ID could end up)</p>
+    <p class="text-white py-2">If you really do want to support me I accept Monero (or XMR, it's the same anyway) & my wallet address is:</p>
+    <code>4AhTY6Xyr92FjiEn7iJ2kvBnrbHbzD8ZQ5CmNKGaJN4haQTpL9MH7Mk4oHdsu6YhMFMrr79UQQuinbVKMkzzsRsCG7SzSZj</code>
 </div>
 <Footer />
