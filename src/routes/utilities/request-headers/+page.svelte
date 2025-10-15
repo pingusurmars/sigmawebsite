15818f47792027145a8fd83a0779d06e1fd4a4b3
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-10-15 18:07:51 +0200

    changed padding & added CF header comment

--- src/routes/utilities/request-headers/+page.svelte
+++ src/routes/utilities/request-headers/+page.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <!-- CF adds headers when passing HTTP requests through their proxy, see https://developers.cloudflare.com/fundamentals/reference/http-headers/ for more info -->
     <p class="text-white text-6xl">My Request Headers ❓</p>
-    <ul class="py-3 list-inline list-inside mx-auto">
+    <ul class="py-4 list-inline list-inside mx-auto">
         {#each data.headers as [key, value]}
             <li class="text-white py-1" ><strong>{key}:</strong> {value}</li>
         {/each}
