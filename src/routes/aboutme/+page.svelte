4e18e499b401806eebdbf21fd80439592782e5e1
PinguSurMars_ <80780388+pingusurmars@users.noreply.github.com>
2025-11-11 12:19:33 +0100

    about me update and projects update

--- src/routes/aboutme/+page.svelte
+++ src/routes/aboutme/+page.svelte
@@ -6,7 +6,7 @@
     <p class="text-white text-6xl">About me 🤔</p>
     <p class="text-white py-4">As you saw in the <a class="underlined" href="/">home page</a>, my name is Pingu(SurMars_), I'm some random teenager on the internet with sleep issues & random productivity spikes at 3 AM. I made this website to show off the stuff I've made/hosted or that I find cool. </p>
     <p class="text-white py-2">For my daily tech, I use an HP Victus 16 s0040-nf laptop as my daily coding/gaming PC. It has a 7000 Series Ryzen 7 7840HS with 16GB of RAM (that I plan on upgrading to 32) & 500GB of NVMe storage (who I also plan on upgrading to 2TB as 500 is NOT enough)</p>
-
+    <p class="text-white py-2">Fun fact: a part of this website was written and uploaded on a Lidl Free Wifi. (yes it's real)</p>
     <p class="text-white py-2">(btw, this website is fully open source, made in Svelte (TS) & hosted on CF Pages/Workers! if you want to check the source code out, check the link that says "here" at the bottom of the page!)</p>
 
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,4 +4,5 @@
 
 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
     <p class="text-white text-6xl">Projects ✨</p>
+    <p class="text-white py-2">As you can see from this page there's nothing really interesting here, come back in some time and I might add stuff here :)</p>
 </div>
