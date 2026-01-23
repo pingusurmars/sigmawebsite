4f12aa7231841f2ff094f1f78683920b5941ac69
PinguSurMars_ <pingusurmars@proton.me>
2026-01-23 14:56:02 +0100

    small modifications and other stuff whatever

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,18 @@
 
 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
     <p class="text-white text-6xl pb-4">Hi!👋</p>
-    <p class="text-white py-4">I'm PinguSurMars_ (or Pingu for short) and I make stuff in whatever language speaks the
-        most to my soul.</p>
-    <p class="text-white py-2">I mostly game or watch YouTube in my free time, and I sometimes decide randomly to code
+    <p class="text-white py-4">
+        I'm PinguSurMars_ (or Pingu for short) and I make stuff in whatever language I feel like using at the time.</p>
+    <p class="text-white py-2">
+        I mostly game or watch YouTube in my free time, and I sometimes decide randomly to code
         random stuff like this website for no particular reason. (I'm writing this at 00:11 CEST on a Wednesday Morning
         with class the next day)</p>
-    <p class="text-white py-2"><a class="underline decoration-1"
-                                  href="https://github.com/pingusurmars/docker-compose-stack">I also run a few
-        services</a> here and there on an old PC that I call my "server" (he's old but works OK-ish for what I do)</p>
-    <p class="text-white py-4">That's pretty much it, feel free to look around here in the other pages! (it's not like I
-        can prevent you from doing it)</p>
+    <p class="text-white py-2"><a class="underline decoration-1" href="https://github.com/pingusurmars/docker-compose-stack">
+        I also run a few services</a> here and there on an old PC that I call my "server" (he's old but works OK-ish for what I do)</p>
+    <p class="text-white py-4">
+        That's pretty much it, feel free to look around here in the other pages! (it's not like I can prevent you from doing it)</p>
 
-    <p class="py-4 text-white">Also here's a random file from my h5ai:</p>
+    <p class="py-4 text-white">Here's a random video I have in my <a class="underline decoration-1" href="https://cdn.pingusurmars.xyz/brainrot/">brainrot collection</a>:</p>
     <video style="width: 20%; height: 20%" src="https://cdn.pingusurmars.xyz/random/" controls>
         <!-- SVELTE PLEASE REMOVE THE SUBTITLE REQUIREMENT NO DEAF PERSON IS GONNA COME UP AND WATCH SHITPOST KNOWING PERFECTLY WELL NOBODY CAPTIONS THAT ANYWAY -->
     </video>
