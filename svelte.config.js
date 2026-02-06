033b2a22bbc406185435479acd259962e8acc9f2
pingusurmars <80780388+pingusurmars@users.noreply.github.com>
2026-02-06 16:12:18 +0100

    prob don't need this anymore

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-
 const config = {
     kit: {
-        adapter: adapter({
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>', '/sitemap.xml']
-            }
-        }),
+        adapter: adapter(),
     }
 };
 
