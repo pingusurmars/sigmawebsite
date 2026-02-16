28f293e45d3f6b3c8f7cc7c09015481697e65ebc
pingusurmars <80780388+pingusurmars@users.noreply.github.com>
2026-02-16 17:44:42 +0100

    added navbar statuspage link

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -68,6 +68,8 @@
                         </div>
                         <a class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"
                            href="/donate/">Donate</a>
+                        <a class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"
+                           href="https://status.pingusurmars.xyz">Status Page</a>
                     </div>
                 </div>
             </div>
